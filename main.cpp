6b6355b15b29a88cade7aa24e97f03df4a274816
unknown <mirkopoj@hotmail.com>
2022-05-03 19:56:25 -0300

    Defines de colores

--- main.cpp
+++ main.cpp
@@ -2,6 +2,26 @@
 #include "Buddy.h"
 #include "Buddy_malloc.h"
 
+#define RST  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+#define FRED(x) KRED x RST
+#define FGRN(x) KGRN x RST
+#define FYEL(x) KYEL x RST
+#define FBLU(x) KBLU x RST
+#define FMAG(x) KMAG x RST
+#define FCYN(x) KCYN x RST
+#define FWHT(x) KWHT x RST
+
+#define BOLD(x) "\x1B[1m" x RST
+#define UNDL(x) "\x1B[4m" x RST
+
 void bloque_libre::Imprimir(){
 	std::cout<<adress<<", ";
 }
@@ -25,6 +45,8 @@ int main(){
 	actualizar_tabla(ptr2, 30, 'B');
 	buddy_free(ptr1);
 	actualizar_tabla(ptr1, 14, '_');
+
+	printf(FWHT("\n%c"),204);
 
 	//IMPRIMIR ESTADO DE LA MEMORIA
 	//temporal, está choto
