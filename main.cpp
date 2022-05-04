763378c572a5934a09ccbfe3a619d235a3eb047d
Mirkopoj <mirkopoj@hotmail.com>
2022-05-04 14:34:29 -0300

    Terminado

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <iostream>
 #include "Buddy.cpp"
 #include "Buddy.h"
@@ -28,25 +29,89 @@ void bloque_libre::Imprimir(){
 }
 
 //TABLA DE ALOCACIONES
-//temporal para printear
 char tabla[MEM] = {0};
+
+//Contadores de memoria
+int MEM_RES=0;
+int MEM_OCP=0;
 
 void actualizar_tabla(int adr, int tam, char nom);
 void tabla_init();
+void print_structs();
 
 int main(){
 	buddy_init();
 	tabla_init();
 
-	int ptr1, ptr2;
+	int ptr1, ptr2, ptr3, ptr4, ptr5;
 
+	print_structs();
 	ptr1 = buddy_malloc(14);
 	actualizar_tabla(ptr1, 14, 'A');
+	print_structs();
 	ptr2 = buddy_malloc(30);
 	actualizar_tabla(ptr2, 30, 'B');
+	print_structs();
 	buddy_free(ptr1);
 	actualizar_tabla(ptr1, 14, '_');
-	
+	print_structs();
+	ptr1 = buddy_malloc(17);
+	actualizar_tabla(ptr1, 17, 'C');
+	print_structs();
+	ptr3 = buddy_malloc(129);
+	actualizar_tabla(ptr3, 129, 'D');
+	print_structs();
+	ptr4 = buddy_malloc(127);
+	actualizar_tabla(ptr4, 127, 'E');
+	print_structs();
+	ptr5 = buddy_malloc(5);
+	actualizar_tabla(ptr5, 5, 'F');
+	print_structs();
+	buddy_free(ptr2);
+	actualizar_tabla(ptr2, 30, '_');
+	print_structs();
+	buddy_free(ptr1);
+	actualizar_tabla(ptr1, 17, '_');
+	print_structs();
+	buddy_free(ptr3);
+	actualizar_tabla(ptr3, 129, '_');
+	print_structs();
+	buddy_free(ptr4);
+	actualizar_tabla(ptr4, 127, '_');
+	print_structs();
+	buddy_free(ptr5);
+	actualizar_tabla(ptr5, 5, '_');
+	print_structs();
+	ptr1 = buddy_malloc(257);
+	actualizar_tabla(ptr1, 257, 'G');
+	print_structs();
+	return 0;
+}
+
+void tabla_init(){
+	for(int i=0; i<MEM; i++){
+		tabla[i]='_';
+	}	
+}
+
+void actualizar_tabla(int adr, int tam, char nom){	//Pone el nombre del bloque alocado para dsp imprimirlo
+	if (nom!='_'){
+		MEM_RES += tam;
+		MEM_OCP += 1<<(redondear(tam));
+	}else{
+		MEM_RES -= tam;
+		tam = 1<<(redondear(tam));
+		MEM_OCP -= tam;
+	}
+	for(int i=adr;i<(adr+tam);i++){
+		tabla[i]=nom;
+	}
+	for(int i=(adr+tam);i<(adr+(1<<redondear(tam)));i++){
+		tabla[i]='#';
+	}
+}
+
+void print_structs(){
 	//IMPRIMIR ESTADO DE LA MEMORIA
 	std::cout<<"MAPA DE MEMORIA"<<std::endl;
 	printf(FWHT("%c"),201);
@@ -73,15 +138,17 @@ int main(){
 		printf(FWHT("%c"),205);	
 	}
 	printf(FWHT("%c\n"),188);
-	printf(FWHT("Espacio Total: %d"),MEM);
-	printf(FRED("Espacio Ocupado: %d"),);
-	printf(FGRN("Espacio Libre: %d"),);
-	printf(FBLU("Espacio Desperdiciado: %d"),);
+	printf(FWHT("Espacio Total: %3d, "),MEM);
+	printf(FWHT("Espacio Ocupado: %3d, "),MEM_OCP);
+	printf(FRED("Espacio Reservado: %3d, "),MEM_RES);
+	printf(FBLU("Espacio Desperdiciado: %3d, "),MEM_OCP-MEM_RES);
+	printf(FGRN("Espacio Libre: %3d\n"),MEM-MEM_OCP);
 
 
 	//IMPRIMIR BITMAP
 	std::cout<<"BITMAP"<<std::endl;
 	printf(FWHT("%c"),201); //primer
+	printf(FWHT("%c%c%c%c"),205,205,205,203);
 	for(int k=0;k<127;k++){
 			printf(FWHT("%c"),205);
 	}
@@ -89,22 +156,26 @@ int main(){
 
 	for(int i=9;i>3;i--){ //6
 		int pad = 1<<(i-3);
+		printf(FWHT("%c%-3d"),186,(1<<i));
 		for(int j=0;j<(MEM/(1<<i));j++){
 			printf("%-*c%-*d", pad, 186, pad, bit_map[i][j]);
 		}
 		printf(FWHT("%c\n"),186);
 		printf(FWHT("%c"),204);
+		printf(FWHT("%c%c%c%c"),205,205,205,206);
 		for(int k=0;k<127;k++){
 			printf(FWHT("%c"),(205-((k%pad)/(pad-1)*2)+(((k%(2*pad))/((2*pad)-1))*3)));
 		}	
 		printf(FWHT("%c\n"),185);
 	}
 
+	printf(FWHT("%c%-3d"),186,8);
 	for(int j=0;j<(MEM/(1<<3));j++){ //ultima
 		printf("%c%d", 186, bit_map[3][j]);
 	}
 	printf(FWHT("%c\n"),186);
 	printf(FWHT("%c"),200);
+	printf(FWHT("%c%c%c%c"),205,205,205,202);
 	for(int k=0;k<127;k++){
 		printf(FWHT("%c"),(205-(k%2)*3));
 	}	
@@ -116,25 +187,6 @@ int main(){
 		std::cout<<i<<":";
 		free_list[i].Recorrer();
 		std::cout<<std::endl;
-	}	
-	return 0;
-}
-
-void tabla_init(){
-	for(int i=0; i<MEM; i++){
-		tabla[i]='_';
-	}	
-}
-
-void actualizar_tabla(int adr, int tam, char nom){	//Pone el nombre del bloque alocado para dsp imprimirlo
-	if (nom=='_'){
-		tam = 1<<(redondear(tam));
-
 	}
-	for(int i=adr;i<(adr+tam);i++){
-		tabla[i]=nom;
-	}
-	for(int i=(adr+tam);i<(adr+(1<<redondear(tam)));i++){
-		tabla[i]='#';
-	}
+	getchar();
 }
